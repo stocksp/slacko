f14ff9298f8334df86889676254fb4b1687d0dc1
Ben Stocks <ben@solenlabs.com>
2024-08-01 20:26:35 -0700

    test cursor conversion of v0 react code to svelte
    sorry about the tailwind

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -19,8 +19,8 @@
 			<li aria-current={$page.url.pathname === '/' ? 'page' : undefined}>
 				<a href="/">Home</a>
 			</li>
-			<li aria-current={$page.url.pathname === '/about' ? 'page' : undefined}>
-				<a href="/about">About</a>
+			<li aria-current={$page.url.pathname === '/ben' ? 'page' : undefined}>
+				<a href="/ben">Ben</a>
 			</li>
 			<li aria-current={$page.url.pathname.startsWith('/sverdle') ? 'page' : undefined}>
 				<a href="/sverdle">Sverdle</a>
---
+++ src/routes/ben/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+	import ChatCard from './ChatCard.svelte';
+</script>
+
+<svelte:head>
+	<title>About</title>
+	<meta name="description" content="About this app" />
+</svelte:head>
+<main>
+	<ChatCard />
+</main>
